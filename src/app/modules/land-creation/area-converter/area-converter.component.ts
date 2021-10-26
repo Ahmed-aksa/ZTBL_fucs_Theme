8e46af86d0a009294089714acadaf167c0ad16cf
behloleaqil <behlole.aqil@aksa-sds.com>
2021-10-26 11:54:05 +0500

    Land Creation First Main component

--- src/app/modules/land-creation/area-converter/area-converter.component.ts
+++ src/app/modules/land-creation/area-converter/area-converter.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-area-converter',
-  templateUrl: './area-converter.component.html',
-  styleUrls: ['./area-converter.component.scss']
+    selector: 'app-area-converter',
+    templateUrl: './area-converter.component.html',
+    styleUrls: ['./area-converter.component.scss']
 })
 export class AreaConverterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    Unit: string;
+    Area: number;
+    ConvertUnit: string;
+    Result: number;
+    ConvertorForm: FormGroup;
+
+    UnitConverter = [{id: "1", name: "Kanal"}, {id: "2", name: "Acre"}, {id: "3", name: "Gunta"}]
+    UnitConverterd = [{id: "1", name: "Marla"}]
 
 }
