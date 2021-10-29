05777bd0478c0a5ab674ccae8b3a4ae18aa31c56
Aksa@4321 <ghazan.usman@aksa-sds.com>
2021-10-29 15:24:42 +0500

    New Functionalty Added in NDC List

--- src/app/modules/ndc-requests/search-ndc-list/search-ndc-list.component.ts
+++ src/app/modules/ndc-requests/search-ndc-list/search-ndc-list.component.ts
@@ -47,14 +47,14 @@ export class SearchNdcListComponent implements OnInit {
     loading: boolean;
     gridHeight: string;
     selected_b;
-    
+
     hide = true;
     selected_z;
     selected_c;
     LoggedInUserInfo:BaseResponseModel;
-  
+
     ndcForm: FormGroup;
-  
+
     pageSize = 10;
     ndcLength : number;
     pendingLength : number;
@@ -62,24 +62,24 @@ export class SearchNdcListComponent implements OnInit {
     offSet = 0;
     count = 1;
     user : any = {};
-  
+
     pageIndex = 1;
     pageIndexPending = 1;
-  
+
     dvReq : any;
     dvPending : any;
-  
+
     Zones: any = [];
     Branches: any = [];
     Circles: any = [];
-  
+
     SelectedZone : any = [];
     SelectedBranch : any = [];
     SelectedCircle : any = [];
     Math: any;
-  
+
     SrNo: number | any;
-  
+
     constructor(
       public dialog: MatDialog,
       private ndc_request_service: NdcRequestsService,
@@ -87,26 +87,26 @@ export class SearchNdcListComponent implements OnInit {
       private fb: FormBuilder,
       private spinner: NgxSpinnerService,
       private userUtilsService: UserUtilsService,) {
-        this.Math = Math; 
+        this.Math = Math;
     }
-  
-    
-  
+
+
+
     ngOnInit() {
       this.LoggedInUserInfo = this.userUtilsService.getUserDetails();
       this.createForm()
-      
-  
+
+
       if (this.LoggedInUserInfo.Branch.BranchCode != "All") {
         this.Circles = this.LoggedInUserInfo.UserCircleMappings;
         this.SelectedCircle = this.Circles;
-  
+
         this.Branches = this.LoggedInUserInfo.Branch;
         this.SelectedBranch = this.Branches;
-  
+
         this.Zones = this.LoggedInUserInfo.Zone;
         this.SelectedZone = this.Zones;
-  
+
         this.selected_z = this.SelectedZone.ZoneId
         this.selected_b = this.SelectedBranch.BranchCode
         this.selected_c = this.SelectedCircle.Id
@@ -114,10 +114,10 @@ export class SearchNdcListComponent implements OnInit {
         this.ndcForm.controls["ZoneId"].setValue(this.SelectedZone.ZoneName);
         this.ndcForm.controls["BranchCode"].setValue(this.SelectedBranch.Name);
       }
-  
+
       this.loadUsersList();
     }
-  
+
     createForm(){
       this.ndcForm = this.fb.group({
         ZoneId: [null],
@@ -126,22 +126,22 @@ export class SearchNdcListComponent implements OnInit {
         Cnic: [null]
       })
     }
-  
+
     applyFilter(filterValue: string) {
       this.dataSource.filter = filterValue;
     }
-  
+
     // getAllNDC(){
     //   this.loadUsersList()
     // }
-  
+
     loadUsersList() {
-      
+
       this.user.ZoneId = this.ndcForm.controls.ZoneId.value;
       this.user.BranchCode = this.ndcForm.controls.BranchCode.value;
       this.user.CircleId = this.ndcForm.controls.CircleId.value;
       this.user.Cnic = this.ndcForm.controls.Cnic.value;
-  
+
       if(this.user.ZoneId != this.SelectedZone.ZoneId && this.user.BranchCode != this.SelectedBranch.BranchCode){
         this.user.ZoneId = this.selected_z;
         this.user.BranchCode = this.selected_b;
@@ -157,13 +157,13 @@ export class SearchNdcListComponent implements OnInit {
         )
         .subscribe((baseResponse: any) => {
           if (baseResponse.Success) {
-            
+
             console.log(baseResponse)
             this.request_data_source = baseResponse.Ndc.Ndcrequests;
             this.dvReq = this.request_data_source;
             this.ndcLength = this.dvReq.length;
             this.request_data_source = this.dvReq.slice(0, this.pageSize);
-  
+
             if (baseResponse.Ndc.pendingNdcs != null) {
               this.pending_requests_data_source = baseResponse.Ndc.pendingNdcs;
               this.dvPending = this.pending_requests_data_source;
@@ -175,26 +175,46 @@ export class SearchNdcListComponent implements OnInit {
           } else {
             this.layoutUtilsService.alertElement('', baseResponse.Message, baseResponse.Code);
           }
-  
+
         });
     }
-  
-  
+
+    DeleteUser(request: any){
+        this.user.NDCId = request.NDCId;
+        this.user.CircleId = request.CircleId;
+        this.user.Remarks = request.MCORemarks;
+
+        // this.user.BranchCode = this.ndcForm.controls.BranchCode.value
+
+        this.ndc_request_service.DeleteCustomer(this.user).pipe( finalize(() =>{
+                this.loading = false;
+                this.spinner.hide();
+        })
+        ).subscribe((baseResponse: any) => {
+            if(baseResponse.Success){
+                this.layoutUtilsService.alertElementSuccess('', baseResponse.Message);
+                window.location.reload();
+            }
+        })
+
+    }
+
+
     ngAfterViewInit() {
-  
+
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
       this.gridHeight = window.innerHeight - 330 + 'px';
     }
-  
+
     // findCnic(cnic: HTMLInputElement) {
     //   this.loadUsersList(cnic.value);
     // }
-  
+
     findCnic() {
       this.loadUsersList();
     }
-  
+
     downloadFile(customer_cnic,customer_id) {
       this.spinner.show();
       this.ndc_request_service.downloadFile(customer_cnic,customer_id, this.pageSize, this.offSet = 0, this.user)
@@ -206,26 +226,62 @@ export class SearchNdcListComponent implements OnInit {
         )
         .subscribe((baseResponse: any) => {
           if (baseResponse.Success) {
-            
+
             this.layoutUtilsService.alertElementSuccess('', baseResponse.Message)
             window.open(baseResponse.Ndc.ndcFilePath);
           }
         });
     }
-  
+
     paginateRequest(pageIndex: any, pageSize: any = this.pageSize) {
       this.pageSize = pageSize;
       this.pageIndex = pageIndex;
       this.offSet = pageIndex;
-      
+
       this.request_data_source = this.dvReq.slice(pageIndex * this.pageSize - this.pageSize, pageIndex * this.pageSize); //slice is used to get limited amount of data from APi
     }
-  
+
     paginatePending(pageIndex: any, pageSize: any = this.pageSize) {
       this.pageIndexPending = pageSize;
       this.pageIndexPending = pageIndex;
       this.offSet = pageIndex;
-      
+
       this.pending_requests_data_source = this.dvPending.slice(pageIndex * this.pageSize - this.pageSize, pageIndex * this.pageSize); //slice is used to get limited amount of data from APi
     }
-  }
+
+    submitDeleteStatus(request) {
+        if (request.LastStatus == 'Submit' || request.LastStatus == 'Pending For Approval' || request.LastStatus == 'ReferBack') {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    submitSubmitStatus(request) {
+        if (request.LastStatus == 'Submit' || request.LastStatus == 'ReferBack') {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    SubmitUser(request) {
+        this.user.NDCId = request.NDCId;
+        this.user.CircleId = request.CircleId;
+        this.user.Remarks = request.MCORemarks;
+
+        // this.user.BranchCode = this.ndcForm.controls.BranchCode.value
+
+        this.ndc_request_service.SubmitUser(this.user).pipe( finalize(() =>{
+                this.loading = false;
+                this.spinner.hide();
+            })
+        ).subscribe((baseResponse: any) => {
+            if(baseResponse.Success){
+                this.layoutUtilsService.alertElementSuccess('', baseResponse.Message);
+                window.location.reload();
+            }
+        })
+    }
+}
