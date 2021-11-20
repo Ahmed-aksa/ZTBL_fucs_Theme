cfe72411eaabb04d96891766ae135e396034915e
behloleaqil <behlole.aqil@aksa-sds.com>
2021-11-20 13:50:57 +0500

    Dynamic branches and zones and cicles in geo fencing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ export const environment = {
     ZTBLUser: 'ZTBLUser',
     isMockEnabled: true, // You have to switch this, when your real back-end is done
     authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-    // apiUrl: 'http://172.16.1.228:8070/ZTBL.Api', //admins
+    apiUrl: 'http://172.16.1.228:8070/ZTBL.Api', //admins
     // apiUrl: 'http://10.250.10.139/ZTBL.Apis', //mustansar_bhai_pc
-    apiUrl: 'http://10.1.103.102:8090/ZTBL.Api', //Ztbl
-    //apiUrl: 'http://10.1.32.105/ZTBL.Api', //ZtblPCMustanser
+    // apiUrl: 'http://10.1.103.102:8090/ZTBL.Api', //Ztbl
+    // apiUrl: 'http://10.1.32.105/ZTBL.Api', //ZtblPCMustanser
     // apiUrl: 'https://localhost:44346/ZTBL.Api', //admins
     //apiUrl: 'http://10.1.32.105/ZTBL.Apis', //ztbl2
 };
