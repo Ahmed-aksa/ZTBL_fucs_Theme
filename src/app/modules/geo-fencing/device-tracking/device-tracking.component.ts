de0e410117aede720970d63061d0e6ab1fc446c1
behloleaqil <behlole.aqil@aksa-sds.com>
2021-11-23 18:14:09 +0500

    Device Locations added page

--- src/app/modules/geo-fencing/device-tracking/device-tracking.component.ts
+++ src/app/modules/geo-fencing/device-tracking/device-tracking.component.ts
@@ -52,13 +52,15 @@ export class DeviceTrackingComponent implements OnInit {
     circle;
     showViewAllBtn: boolean;
 
+    device_locations: any;
+
     constructor(
         private _circleService: CircleService,
         private layoutUtilsService: LayoutUtilsService,
         private _cdf: ChangeDetectorRef,
         private fb: FormBuilder,
         private spinner: NgxSpinnerService,
-        public _geoFencingService:GeoFencingService
+        public _geoFencingService: GeoFencingService
     ) {
     }
 
@@ -85,7 +87,6 @@ export class DeviceTrackingComponent implements OnInit {
     ngAfterViewInit() {
         this.gridHeight = window.innerHeight - 250 + 'px';
     }
-
 
 
     loadCirclesSinglePoints() {
@@ -132,21 +133,16 @@ export class DeviceTrackingComponent implements OnInit {
         return 'View Circle Fense';
     }
 
-
-    ///////////////////Os Change Set Map
     onMapReady(map) {
         this.googleMap = map;
         this.setCurrentLocation()
-        if(this.zone && this.branch)
-        {
-        this.loadCirclesSinglePoints();
+        if (this.zone && this.branch) {
+            this.loadCirclesSinglePoints();
         }
     }
 
 
     clickedMarker(index: number, infowindow) {
-
-        this.viewCircleFense(index);
         if (this.previousInfoWindow != null) {
             this.previousInfoWindow.close();
         }
@@ -173,8 +169,7 @@ export class DeviceTrackingComponent implements OnInit {
 
     }
 
-    find()
-    {
+    find() {
 
         // this.loadCirclesSinglePoints();
 
@@ -197,96 +192,13 @@ export class DeviceTrackingComponent implements OnInit {
             this.spinner.hide();
 
             if (baseResponse.Success === true) {
-console.log(JSON.stringify(baseResponse))
-                // this.geo_fence_points = baseResponse.GeoFancPoint.GeoFancPoints;
+                this.device_locations = baseResponse.LocationHistory.AgentLocations;
 
             } else {
-
+                this.device_locations = [];
                 this.layoutUtilsService.alertElement("", baseResponse.Message);
             }
         });
-    }
-    data
-    GetGeoFancPoint() {
-        this.spinner.show()
-        this._geoFencingService.GetGeoFancPoint(this.data)
-            .pipe(
-                finalize(() => {
-                    this.spinner.hide();
-                })).subscribe((baseResponse: BaseResponseModel) => {
-            if (baseResponse.Success === true) {
-                // this.LocationHistories = baseResponse.LocationHistory["LocationHistories"]
-                // this.latlong = JSON.parse(baseResponse.LocationHistory.LocationHistories[0]["LocationData"]);
-                // this.start_end_mark.push(this.latlong[0]);
-                // this.start_end_mark.push(this.latlong[this.latlng?.length - 1]);
-                //
-                // this.detector.detectChanges();
-            } else {
-                this.layoutUtilsService.alertElement("", baseResponse.Message);
-            }
-        });
-    }
-
-    viewCircleFense = (index: number) => {
-        if (this.isLoadingFence) {
-            return
-        }
-        this.isLoadingFence = true;
-        var circle = new Circle();
-        circle.Id = this.fenceMarkers[index].CircleId
-
-        //this.removeClickedMarker(index)
-
-        this._circleService.CirclePoligonGet(circle)
-            .pipe(
-                finalize(() => {
-                    this.loading = false;
-                })
-            )
-            .subscribe(baseResponse => {
-                if (baseResponse.Success) {
-
-                    var OldFancPoints = baseResponse.GeoFancPoints;
-
-                    const polygonCoordinate = [];
-                    OldFancPoints.forEach((o, i) => {
-                        if (o.Long != 0 && o.Lat != 0) {
-                            // for view only
-                            polygonCoordinate.push({
-                                lat: o.Lat,
-                                lng: o.Long,
-                            });
-                        }
-                    });
-                    if (polygonCoordinate.length > 0) {
-                        this.deleteSelectedShape()
-                        this.selectedShape = new google.maps.Polygon({
-                            paths: polygonCoordinate,
-                            strokeColor: this.viewColor,
-                            strokeOpacity: 0.8,
-                            strokeWeight: 2,
-                            fillColor: this.viewColor,
-                            fillOpacity: 0.35,
-                        });
-
-                        this.selectedShape.setMap(this.googleMap);
-
-                        if (this.zoom < 12) {
-                            this.lat = polygonCoordinate[1].lat;
-                            this.lng = polygonCoordinate[1].lng;
-                            this.zoom = 12;
-                        }
-
-                    } else {
-                        this.layoutUtilsService.alertElement("", "No fense created against this circle", baseResponse.Code);
-                    }
-                    this.isLoadingFence = false
-                } else {
-                    this.isLoadingFence = false
-                    this.layoutUtilsService.alertElement("", baseResponse.Message, baseResponse.Code);
-                }
-
-            });
     }
 
 
