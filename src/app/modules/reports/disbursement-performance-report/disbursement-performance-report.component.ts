b31eb24f652a7c77061e4316602bd6d1862b25d7
abdulrehmanaxa1 <abdul.rehman@axabiztech.com>
2021-12-02 11:31:42 +0500

    2 new reports

---
+++ src/app/modules/reports/disbursement-performance-report/disbursement-performance-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disbursement-performance-report',
+  templateUrl: './disbursement-performance-report.component.html',
+  styleUrls: ['./disbursement-performance-report.component.scss']
+})
+export class DisbursementPerformanceReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
