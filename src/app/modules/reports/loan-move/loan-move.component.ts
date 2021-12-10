e299d63d4ac2d02e6d6ca757aaf998d2e907bc6f
abdulrehmanaxa1 <abdul.rehman@axabiztech.com>
2021-12-10 11:53:36 +0500

    3 reports tested

--- src/app/modules/reports/loan-move/loan-move.component.ts
+++ src/app/modules/reports/loan-move/loan-move.component.ts
@@ -1,15 +1,237 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {BaseResponseModel} from "../../../shared/models/base_response.model";
+import {Bufrication} from "../class/reports";
+import {MatDialogRef} from "@angular/material/dialog";
+import {UserUtilsService} from "../../../shared/services/users_utils.service";
+import {LovService} from "../../../shared/services/lov.service";
+import {ReportsService} from "../service/reports.service";
+import {LayoutUtilsService} from "../../../shared/services/layout_utils.service";
+import {NgxSpinnerService} from "ngx-spinner";
+import {ToastrService} from "ngx-toastr";
+import {DateFormats, Lov, LovConfigurationKey} from "../../../shared/classes/lov.class";
+import {finalize} from "rxjs/operators";
+import {DatePipe} from "@angular/common";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import {MomentDateAdapter} from "@angular/material-moment-adapter";
 
 @Component({
-  selector: 'app-loan-move',
-  templateUrl: './loan-move.component.html',
-  styleUrls: ['./loan-move.component.scss']
+    selector: 'app-loan-move',
+    templateUrl: './loan-move.component.html',
+    styleUrls: ['./loan-move.component.scss'],
+    providers: [
+        DatePipe,
+        {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+        {provide: MAT_DATE_FORMATS, useValue: DateFormats}
+
+    ],
 })
+
 export class LoanMoveComponent implements OnInit {
 
-  constructor() { }
+    bufricationForm: FormGroup;
+    selected_b;
+    selected_z;
+    selected_c;
+    loaded = true;
+    disable_circle = true;
+    disable_zone = true;
+    disable_branch = true;
+    single_branch = true;
+    single_circle = true;
+    single_zone = true;
+    LoggedInUserInfo: BaseResponseModel;
+    statusLov: any;
+    loading = false;
 
-  ngOnInit(): void {
-  }
+    public reports = new Bufrication();
+
+
+    //Zone inventory
+    Zones: any = [];
+    SelectedZones: any = [];
+
+    final_branch: any;
+    final_zone: any;
+
+    branch: any;
+    zone: any;
+    circle: any;
+
+    dateDisable: boolean = false;
+    today = new Date();
+
+
+    //Branch inventory
+    Branches: any = [];
+    SelectedBranches: any = [];
+
+    //Circle inventory
+    Circles: any = [];
+    SelectedCircles: any = [];
+
+    user: any = {}
+
+    constructor(
+        private dialogRef: MatDialogRef<LoanMoveComponent>,
+        private fb: FormBuilder,
+        private userUtilsService: UserUtilsService,
+        private _lovService: LovService,
+        private _bufrication: ReportsService,
+        private layoutUtilsService: LayoutUtilsService,
+        private spinner: NgxSpinnerService,
+        private toastr: ToastrService,
+        private _reports: ReportsService
+    ) {
+    }
+
+    public LovCall = new Lov();
+
+
+    ngOnInit(): void {
+        this.LoggedInUserInfo = this.userUtilsService.getSearchResultsDataOfZonesBranchCircle();
+        this.createForm();
+
+        if (this.LoggedInUserInfo.Branch.WorkingDate) {
+            let dateString = this.LoggedInUserInfo.Branch.WorkingDate;
+            var day = parseInt(dateString.substring(0, 2));
+            var month = parseInt(dateString.substring(2, 4));
+            var year = parseInt(dateString.substring(4, 8));
+
+            this.today = new Date(year, month - 1, day);
+            //const branchWorkingDate = new Date(year, month - 1, day);
+            this.bufricationForm.controls.WorkingDate.setValue(this.today);
+            this.dateDisable = true
+        } else {
+            this.bufricationForm.controls.WorkingDate.setValue(null);
+        }
+    }
+
+    isEnableWorkingDate() {
+        var workingDate = this.bufricationForm.controls.WorkingDate.value;
+        if (workingDate._isAMomentObject == undefined) {
+            try {
+                var day = this.bufricationForm.controls.WorkingDate.value.getDate();
+                var month = this.bufricationForm.controls.WorkingDate.value.getMonth() + 1;
+                var year = this.bufricationForm.controls.WorkingDate.value.getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                const branchWorkingDate = new Date(year, month - 1, day);
+
+                this.bufricationForm.controls.WorkingDate.setValue(branchWorkingDate);
+            } catch (e) {
+
+            }
+        } else {
+            try {
+                var day = this.bufricationForm.controls.WorkingDate.value.toDate().getDate();
+                var month = this.bufricationForm.controls.WorkingDate.value.toDate().getMonth() + 1;
+                var year = this.bufricationForm.controls.WorkingDate.value.toDate().getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                workingDate = day + "" + month + "" + year;
+
+
+                const branchWorkingDate = new Date(year, month - 1, day);
+                this.bufricationForm.controls.WorkingDate.setValue(branchWorkingDate);
+            } catch (e) {
+
+            }
+        }
+    }
+
+
+    createForm() {
+        this.bufricationForm = this.fb.group({
+            WorkingDate: [null, Validators.required],
+            LcNO: [null],
+        })
+    }
+
+
+    find() {
+
+        if (this.bufricationForm.invalid) {
+            this.toastr.error("Please Enter Required values");
+            return;
+        }
+        this.reports = Object.assign(this.reports, this.bufricationForm.value);
+        this.reports.ReportsNo = "7";
+        this.reports.ReportFormatType = "2";
+        var myWorkingDate = this.bufricationForm.controls.WorkingDate.value;
+        if (myWorkingDate._isAMomentObject == undefined) {
+
+            try {
+                var day = this.bufricationForm.controls.WorkingDate.value.getDate();
+                var month = this.bufricationForm.controls.WorkingDate.value.getMonth() + 1;
+                var year = this.bufricationForm.controls.WorkingDate.value.getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                myWorkingDate.WorkingDate = day + "" + month + "" + year;
+                this.reports.WorkingDate = myWorkingDate.WorkingDate;
+            } catch (e) {
+
+            }
+        } else {
+            try {
+                var day = this.bufricationForm.controls.WorkingDate.value.toDate().getDate();
+                var month = this.bufricationForm.controls.WorkingDate.value.toDate().getMonth() + 1;
+                var year = this.bufricationForm.controls.WorkingDate.value.toDate().getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                this.reports.WorkingDate = day + "" + month + "" + year;
+            } catch (e) {
+
+            }
+        }
+
+        if (this.branch.WorkingDate == undefined) {
+            this.branch.WorkingDate = this.reports.WorkingDate;
+        }
+
+        this.spinner.show();
+        this._reports.reportDynamic(this.reports, this.zone, this.branch)
+            .pipe(
+                finalize(() => {
+                    this.loaded = true;
+                    this.loading = false;
+                    this.spinner.hide();
+                })
+            )
+            .subscribe((baseResponse: any) => {
+                if (baseResponse.Success === true) {
+                    window.open(baseResponse.ReportsFilterCustom.FilePath, 'Download');
+                } else {
+                    this.layoutUtilsService.alertElement("", baseResponse.Message);
+                }
+            })
+    }
+
+    getAllData(data) {
+        this.zone = data.final_zone;
+        this.branch = data.final_branch;
+        this.circle = null;
+    }
+
+    close(res) {
+        this.dialogRef.close(res)
+    }
+
 
 }
--- src/app/modules/reports/mark-on-ca/mark-on-ca.component.ts
+++ src/app/modules/reports/mark-on-ca/mark-on-ca.component.ts
@@ -1,15 +1,225 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {BaseResponseModel} from "../../../shared/models/base_response.model";
+import {Bufrication} from "../class/reports";
+import {MatDialogRef} from "@angular/material/dialog";
+import {UserUtilsService} from "../../../shared/services/users_utils.service";
+import {LovService} from "../../../shared/services/lov.service";
+import {LayoutUtilsService} from "../../../shared/services/layout_utils.service";
+import {NgxSpinnerService} from "ngx-spinner";
+import {ToastrService} from "ngx-toastr";
+import {DatePipe} from "@angular/common";
+import {ReportsService} from "../service/reports.service";
+import {DateFormats, Lov, LovConfigurationKey} from "../../../shared/classes/lov.class";
+import {MatCheckboxChange} from "@angular/material/checkbox";
+import {finalize} from "rxjs/operators";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import {MomentDateAdapter} from "@angular/material-moment-adapter";
 
 @Component({
-  selector: 'app-mark-on-ca',
-  templateUrl: './mark-on-ca.component.html',
-  styleUrls: ['./mark-on-ca.component.scss']
+    selector: 'app-mark-on-ca',
+    templateUrl: './mark-on-ca.component.html',
+    styleUrls: ['./mark-on-ca.component.scss'],
+    providers: [
+        DatePipe,
+        {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+        {provide: MAT_DATE_FORMATS, useValue: DateFormats}
+
+    ],
 })
+
 export class MarkOnCaComponent implements OnInit {
 
-  constructor() { }
+    bufricationForm: FormGroup;
+    loaded = true;
+    LoggedInUserInfo: BaseResponseModel;
 
-  ngOnInit(): void {
-  }
+
+    dateCheck = new Date();
+
+    loading = false;
+
+    public reports = new Bufrication();
+    branch: any;
+    zone: any;
+    circle: any;
+    user: any = {}
+
+    //getWorkingDate = WorkingDate()
+    constructor(
+        private dialogRef: MatDialogRef<MarkOnCaComponent>,
+        private fb: FormBuilder,
+        private userUtilsService: UserUtilsService,
+        private _lovService: LovService,
+        private layoutUtilsService: LayoutUtilsService,
+        private spinner: NgxSpinnerService,
+        private toastr: ToastrService,
+        private datepipe: DatePipe,
+        private _reports: ReportsService
+    ) {
+    }
+
+    public LovCall = new Lov();
+
+    ngOnInit(): void {
+        this.LoggedInUserInfo = this.userUtilsService.getSearchResultsDataOfZonesBranchCircle();
+        this.createForm();
+
+        if (this.branch) {
+            let dateString = this.branch.WorkingDate;
+            var day = parseInt(dateString.substring(0, 2));
+            var month = parseInt(dateString.substring(2, 4));
+            var year = parseInt(dateString.substring(4, 8));
+
+            const branchWorkingDate = new Date(year, month - 1, day);
+            this.bufricationForm.controls.ToDate.setValue(branchWorkingDate);
+            this.bufricationForm.controls.FromDate.setValue(branchWorkingDate);
+        } else {
+            let dateString = "11012021";
+            var day = parseInt(dateString.substring(0, 2));
+            var month = parseInt(dateString.substring(2, 4));
+            var year = parseInt(dateString.substring(4, 8));
+
+            const branchWorkingDate = new Date(year, month - 1, day);
+            this.bufricationForm.controls.ToDate.setValue(branchWorkingDate);
+            this.bufricationForm.controls.FromDate.setValue(branchWorkingDate);
+        }
+    }
+
+
+    isEnableToDate() {
+        var toDate = this.bufricationForm.controls.ToDate.value;
+        if (toDate._isAMomentObject == undefined) {
+            try {
+                var day = this.bufricationForm.controls.ToDate.value.getDate();
+                var month = this.bufricationForm.controls.ToDate.value.getMonth() + 1;
+                var year = this.bufricationForm.controls.ToDate.value.getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                const branchToDate = new Date(year, month - 1, day);
+                this.bufricationForm.controls.ToDate.setValue(branchToDate);
+            } catch (e) {
+
+            }
+        } else {
+            try {
+                var day = this.bufricationForm.controls.ToDate.value.toDate().getDate();
+                var month = this.bufricationForm.controls.ToDate.value.toDate().getMonth() + 1;
+                var year = this.bufricationForm.controls.ToDate.value.toDate().getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                toDate = day + "" + month + "" + year;
+
+
+                const branchToDate = new Date(year, month - 1, day);
+                this.bufricationForm.controls.ToDate.setValue(branchToDate);
+            } catch (e) {
+
+            }
+        }
+    }
+
+    isEnableFromDate() {
+        var toDate = this.bufricationForm.controls.FromDate.value;
+        if (toDate._isAMomentObject == undefined) {
+            try {
+                var day = this.bufricationForm.controls.FromDate.value.getDate();
+                var month = this.bufricationForm.controls.FromDate.value.getMonth() + 1;
+                var year = this.bufricationForm.controls.FromDate.value.getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                const branchToDate = new Date(year, month - 1, day);
+                this.bufricationForm.controls.FromDate.setValue(branchToDate);
+            } catch (e) {
+
+            }
+        } else {
+            try {
+                var day = this.bufricationForm.controls.FromDate.value.toDate().getDate();
+                var month = this.bufricationForm.controls.FromDate.value.toDate().getMonth() + 1;
+                var year = this.bufricationForm.controls.FromDate.value.toDate().getFullYear();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (day < 10) {
+                    day = "0" + day;
+                }
+                toDate = day + "" + month + "" + year;
+
+
+                const branchToDate = new Date(year, month - 1, day);
+                this.bufricationForm.controls.FromDate.setValue(branchToDate);
+            } catch (e) {
+
+            }
+        }
+    }
+
+
+    createForm() {
+        this.bufricationForm = this.fb.group({
+            FromDate: [null, Validators.required],
+            ToDate: [null, Validators.required]
+        })
+    }
+
+
+    find() {
+        debugger
+        if (this.bufricationForm.invalid) {
+            this.toastr.error("Please Enter Required values");
+            return;
+        }
+
+        this.reports = Object.assign(this.reports, this.bufricationForm.value);
+        this.reports.ReportsNo = "3";
+
+        var toDate = this.datepipe.transform(this.bufricationForm.controls.ToDate.value, 'ddMMyyyy');
+
+        var fromDate = this.datepipe.transform(this.bufricationForm.controls.FromDate.value, 'ddMMyyyy');
+        this.reports.ToDate = toDate;
+        this.reports.FromDate = fromDate;
+        this.reports.ReportFormatType = "2";
+
+
+        this.spinner.show();
+        this._reports.reportDynamic(this.reports, this.zone, this.branch)
+            .pipe(
+                finalize(() => {
+                    this.loaded = true;
+                    this.loading = false;
+                    this.spinner.hide();
+                })
+            )
+            .subscribe((baseResponse: any) => {
+                if (baseResponse.Success === true) {
+                    window.open(baseResponse.ReportsFilterCustom.FilePath, 'Download');
+                } else {
+                    this.layoutUtilsService.alertElement("", baseResponse.Message);
+                }
+            })
+    }
+
+    getAllData(data) {
+        this.zone = data.final_zone;
+        this.branch = data.final_branch;
+        this.circle = data.final_circle;
+    }
+
+    close(res) {
+        this.dialogRef.close(res)
+    }
 
 }
