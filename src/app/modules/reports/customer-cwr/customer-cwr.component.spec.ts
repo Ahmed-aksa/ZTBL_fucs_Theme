021ddd8696fe80a290491e21b8592fd2dfe35814
abdulrehmanaxa1 <abdul.rehman@axabiztech.com>
2021-12-21 15:55:20 +0500

    new report

---
+++ src/app/modules/reports/customer-cwr/customer-cwr.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomerCwrComponent } from './customer-cwr.component';
+
+describe('CustomerCwrComponent', () => {
+  let component: CustomerCwrComponent;
+  let fixture: ComponentFixture<CustomerCwrComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CustomerCwrComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustomerCwrComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
