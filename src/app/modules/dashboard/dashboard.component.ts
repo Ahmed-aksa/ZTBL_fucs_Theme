494880d5e7e5b27de9c083a0c762acca98416ff4
behloleaqil <behlole.aqil@aksa-sds.com>
2021-12-21 16:12:04 +0500

    Index html modified

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
-import { Component, HostListener, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { MatTableDataSource } from "@angular/material/table";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import { SessionExpireService } from 'app/shared/services/session-expire.service';
-import { Router } from '@angular/router';
-import { ThrowStmt } from '@angular/compiler';
-import { NgxSpinnerService } from 'ngx-spinner';
+import {Component, HostListener, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {SessionExpireService} from 'app/shared/services/session-expire.service';
+import {Router} from '@angular/router';
+import {ThrowStmt} from '@angular/compiler';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {finalize} from "rxjs/operators";
+import {DashboardService} from "../../shared/services/dashboard.service";
+import {ClApplicationHeaderComponent} from "../loan/cl-application-header/cl-application-header.component";
+import {ClCustomersComponent} from "../loan/cl-customers/cl-customers.component";
+import {ClSecuritiesComponent} from "../loan/cl-securities/cl-securities.component";
+import {ClLegalHeirsComponent} from "../loan/cl-legal-heirs/cl-legal-heirs.component";
+import {ClPurposeComponent} from "../loan/cl-purpose/cl-purpose.component";
+import {ClLoanWitnessComponent} from "../loan/cl-loan-witness/cl-loan-witness.component";
+import {McoDashboradComponent} from "./mco-dashborad/mco-dashborad.component";
+import {RecoveryOfficerDashboardComponent} from "./recovery-officer-dashboard/recovery-officer-dashboard.component";
 
 @Component({
     selector: 'app-dashboard',
@@ -14,19 +24,23 @@ import { NgxSpinnerService } from 'ngx-spinner';
     encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
+
+    @ViewChild(McoDashboradComponent, {static: false}) mcoDashboardComponent: McoDashboradComponent;
+    @ViewChild(RecoveryOfficerDashboardComponent, {static: false}) recoveryOfficerDashboardComponent: RecoveryOfficerDashboardComponent;
     popup: any = false;
     time: any
     sessionTime: any;
-    userGroup:any=[];
+    userGroup: any = [];
+    rolesData: any;
 
-    constructor(private _sessionExpireService: SessionExpireService, private _router: Router, private spinner:NgxSpinnerService) {
-    this.spinner.show();
+    constructor(private _sessionExpireService: SessionExpireService, private _router: Router, private spinner: NgxSpinnerService, private _dashboardService: DashboardService) {
+        this.spinner.show();
     }
 
     dataSource = new MatTableDataSource();
     displayedColumns = ['EmployeeNo', 'EmployeeName', 'PhoneNumber', 'Email', 'ZoneName', 'BranchName', 'UserCircles', 'actions'];
-    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-    @ViewChild(MatSort, { static: true }) sort: MatSort;
+    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+    @ViewChild(MatSort, {static: true}) sort: MatSort;
 
     ngOnInit(): void {
         this.setSessionTime();
@@ -43,6 +57,14 @@ export class DashboardComponent implements OnInit {
 
             this.time = c.toString();
         });
+
+        this._dashboardService.getDashboardData().pipe(finalize(() => {
+            this.spinner.hide()
+        })).subscribe(result => {
+            this.mcoDashboardComponent.assignRoleData(result.DashboardReport);
+            this.recoveryOfficerDashboardComponent.assignRoleData(result.DashboardReport);
+        });
+
     }
 
     setSessionTime() {
@@ -74,7 +96,7 @@ export class DashboardComponent implements OnInit {
 
     @HostListener('window:keydown')
     @HostListener('window:mousedown')
-   // @HostListener('mousemove')
+    // @HostListener('mousemove')
     checkUserActivity() {
         this.setSessionTime();
     }
