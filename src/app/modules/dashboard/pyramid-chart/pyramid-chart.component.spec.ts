78b645f99e19126b2bd14a407cbfa8745d238285
ahmed.javed <ahmed.javed@aksa-sds.com>
2021-12-22 17:06:22 +0500

    Dashborad

---
+++ src/app/modules/dashboard/pyramid-chart/pyramid-chart.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PyramidChartComponent } from './pyramid-chart.component';
+
+describe('PyramidChartComponent', () => {
+  let component: PyramidChartComponent;
+  let fixture: ComponentFixture<PyramidChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PyramidChartComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PyramidChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
