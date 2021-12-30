50b1f32943cb6946adc9f7ca727ae6bdd011f3fc
Ahmed-aksa <92078954+Ahmed-aksa@users.noreply.github.com>
2021-12-31 01:28:46 +0500

    encription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationError, NavigationStart, Router, Event } from '@angular/router';
 import { environment } from 'environments/environment';
-
+import * as Forge from 'node-forge';
+import * as CryptoJS from 'crypto-js';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -57,21 +58,62 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
 
-        // console.log(this.newGuid());
-        // var valueToEncrypt = "Ahmed Javed"
-        // var date = {
-        //     id: '1',
-        //     Name: 'ahmed Javed',
-        //   };
-        // const rsa = Forge.pki.publicKeyFromPem(publicKey);
-        // let enr = window.btoa(rsa.encrypt(JSON.stringify(date)));
-        // console.log(window.atob(enr));
+        let key = this.getNewKey();
+        var data = {
+            "Circle": {
+                "CircleIds": ""
+            },
+            "DeviceLocation": {
+                "Accuracy": 0.0,
+                "BtsId": "0",
+                "BtsLoc": "",
+                "Date": "0",
+                "id": 0,
+                "Lat": "33.6981501",
+                "Long": "73.0639128",
+                "Speed": 0.0,
+                "Src": "GPS",
+                "StartId": 0,
+                "Status": -1,
+                "Time": "0"
+            },
+            "doPerformOTP": false,
+            "Key": key,
+            "TranId": 0,
+            "User": {
+                "IsActive": 0,
+                "App": 1,
+                "Channel": "user",
+                "ChannelID": 0,
+                "Lat": 0.0,
+                "Long": 0.0,
+                "ProfileID": 0,
+                "UserId": "",
+                "UserIp": "10.1.32.157",
+                "UserType": 0
+            }
+        }
+        debugger;
+        var responseEncript = this.set(data, key);
+        console.log(responseEncript);
+
+        const rsa = Forge.pki.publicKeyFromPem(environment.publicRSAKey);
+        let rsaEnrkey = rsa.encrypt(key.toString(),"RAW")
+        debugger;
+        console.log(rsaEnrkey.toString());
+
         // debugger;
         // let res = Forge.pki.privateKeyFromPem(privateKey);
 
-        // console.log(res.decrypt(window.atob(enr)))
+        // console.log(res.decrypt(window.atob(enr)));
 
-        // this.http.post(`${environment.apiUrl}/Account/HealthCheck`,"").subscribe(result=>{
+        var cusomeRequestModel = {
+                "Key":rsaEnrkey,
+                "Req":responseEncript
+        }
+
+        // this.http.post(`${environment.apiUrl}/Account/HealthCheck`,cusomeRequestModel).subscribe(result=>{
+
         // });
 
     }
@@ -90,7 +132,45 @@ export class AppComponent implements OnInit {
             return v.toString(16);
         });
     }
+
+
+    set(keys, value) {
+        var key = CryptoJS.enc.Utf8.parse(keys);
+        var iv = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        var iv1 = CryptoJS.lib.WordArray.create(iv);
+        var encrypted = CryptoJS.AES.encrypt(
+            CryptoJS.enc.Utf8.parse(JSON.stringify(value)),
+            key,
+            {
+                keySize: 256 / 8,
+                iv: iv1,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7,
+            }
+        );
+
+        return encrypted.toString();
+    }
+
+    //The get method is use for decrypt the value.
+    get(keys, value) {
+        var key = CryptoJS.enc.Utf8.parse(keys);
+        var iv = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        var iv1 = CryptoJS.lib.WordArray.create(iv);
+        var decrypted = CryptoJS.AES.decrypt(value, key, {
+            keySize: 256 / 8,
+            iv: iv1,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7,
+        });
+        return decrypted.toString(CryptoJS.enc.Utf8);
+    }
+
+
+
 }
+
+
 export const privateKey = `-----BEGIN RSA PRIVATE KEY-----
 MIICXQIBAAKBgQDObZtjxfplZYRgo6TKZM9E6b3RVQpXTpKTOiqefTKEpT9//ru1
 x0rHgqpsjcw1BoXFX3SuYRPn3ijCM/C9WHnc2PDjEgGu0KezIxvqE7nCjbHed7pf
