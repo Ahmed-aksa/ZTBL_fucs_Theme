cf75160de322163ca578bb7cde25a0d6e4ecff3f
abdulrehmanaxa1 <abdul.rehman@axabiztech.com>
2021-12-31 18:49:09 +0500

    UAT Fix

--- src/app/shared/services/dashboard.service.ts
+++ src/app/shared/services/dashboard.service.ts
@@ -1,15 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { BaseRequestModel } from '../models/base_request.model';
-import { BaseResponseModel } from '../models/base_response.model';
-import { HttpUtilsService } from './http_utils.service';
-import { UserUtilsService } from './users_utils.service';
-import { ChartOptions } from "../../modules/dashboard/evp-credit-dashboard/evp-credit-dashboard.component";
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {environment} from 'environments/environment';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {BaseRequestModel} from '../models/base_request.model';
+import {BaseResponseModel} from '../models/base_response.model';
+import {HttpUtilsService} from './http_utils.service';
+import {UserUtilsService} from './users_utils.service';
+import {ChartOptions} from "../../modules/dashboard/evp-credit-dashboard/evp-credit-dashboard.component";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class DashboardService {
     public request = new BaseRequestModel();
 
@@ -47,54 +47,56 @@ export class DashboardService {
             .post(
                 `${environment.apiUrl}/Dashboard/GetDashboardReport`,
                 this.request,
-                { headers: this.httpUtils.getHTTPHeaders() }
+                {headers: this.httpUtils.getHTTPHeaders()}
             )
             .pipe(map((res: any) => res));
     }
 
     assignKeys(data: any, title): Partial<ChartOptions> {
-        var obj = [];
-        (Object.values(data)).forEach(x => {
-            obj.push(Number(x));
+        if (data) {
+            var obj = [];
+            if (data)
+                (Object.values(data))?.forEach(x => {
+                    obj.push(Number(x));
 
-        });
-       
-        return {
-            series: obj,
-            chart: {
-                width: "100%",
-                type: "pie"
-            },
-            labels:  Object.keys(data).map(key => key=key.replace(/([a-z0-9])([A-Z])/g, '$1 $2')),
-            theme: {
-                monochrome: {
-                    enabled: false
-                }
-            },
-            title: {
-                text: title
-            },
-            responsive: [
-                {
-                    breakpoint: 480,
-                    options: {
-                        chart: {
-                            width: 200
-                        },
-                        legend: {
-                            position: "bottom"
+                });
+            return {
+                series: obj,
+                chart: {
+                    width: "100%",
+                    type: "pie"
+                },
+                labels: Object.keys(data)?.map(key => key = key.replace(/([a-z0-9])([A-Z])/g, '$1 $2')),
+                theme: {
+                    monochrome: {
+                        enabled: false
+                    }
+                },
+                title: {
+                    text: title
+                },
+                responsive: [
+                    {
+                        breakpoint: 480,
+                        options: {
+                            chart: {
+                                width: 200
+                            },
+                            legend: {
+                                position: "bottom"
+                            }
                         }
                     }
+                ],
+                noData: {
+                    text: "There's no data",
+                    align: 'center',
+                    verticalAlign: 'middle',
+                    offsetX: 0,
+                    offsetY: 0
                 }
-            ],
-            noData: {
-                text: "There's no data",
-                align: 'center',
-                verticalAlign: 'middle',
-                offsetX: 0,
-                offsetY: 0
-              }
-        };
+            };
+        }
     }
 
 }
