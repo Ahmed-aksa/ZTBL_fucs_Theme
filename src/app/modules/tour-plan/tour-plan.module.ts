d24ddeae96d99b93a160ffded6bcbb8d6bb5f815
behloleaqil <behlole.aqil@aksa-sds.com>
2022-01-13 14:33:14 +0500

    Tour Plan for approval

--- src/app/modules/tour-plan/tour-plan.module.ts
+++ src/app/modules/tour-plan/tour-plan.module.ts
@@ -4,10 +4,12 @@ import {SharedModule} from 'app/shared/shared.module';
 import {RouterModule} from '@angular/router';
 import {TourPlanComponent} from "./tour-plan/tour-plan.component";
 import {SearchTourPlanComponent} from "./search-tour-plan/search-tour-plan.component";
-import { CreateTourLlanComponent } from './create-tour-plan/create-tour-plan.component';
-import { CreateTourPlanPopupComponent } from './create-tour-plan/create-tour-plan-popup/create-tour-plan-popup.component';
-import { TargetsHierarchyComponent } from './targets-hierarchy/targets-hierarchy.component';
-import { TargetsTrackingComponent } from './targets-tracking/targets-tracking.component';
+import {CreateTourLlanComponent} from './create-tour-plan/create-tour-plan.component';
+import {CreateTourPlanPopupComponent} from './create-tour-plan/create-tour-plan-popup/create-tour-plan-popup.component';
+import {TargetsHierarchyComponent} from './targets-hierarchy/targets-hierarchy.component';
+import {TargetsTrackingComponent} from './targets-tracking/targets-tracking.component';
+import { TourPlanForApprovalComponent } from './tour-plan-for-approval/tour-plan-for-approval.component';
+
 export let AppInjector: Injector;
 const routing = [
 
@@ -31,10 +33,10 @@ const routing = [
         path: "target-tracking",
         component: TargetsTrackingComponent,
     },
-    // {
-    //     path: "tour-plan-for-approval",
-    //     component: TourPlanForApprovalComponent,
-    // },
+    {
+        path: "tour-plan-for-approval",
+        component: TourPlanForApprovalComponent,
+    },
 ]
 
 @NgModule({
@@ -43,7 +45,8 @@ const routing = [
         SearchTourPlanComponent,
         CreateTourLlanComponent,
         CreateTourPlanPopupComponent,
-        TargetsTrackingComponent
+        TargetsTrackingComponent,
+        TourPlanForApprovalComponent
         // TourPlanForApprovalComponent
     ],
     imports: [
@@ -56,10 +59,13 @@ const routing = [
     ],
     schemas: [
         CUSTOM_ELEMENTS_SCHEMA
+    ],
+    exports: [
+        // AppInjector,
     ]
 })
 export class TourPlanModule {
     constructor(private injector: Injector) {
         AppInjector = this.injector;
-      }
+    }
 }
