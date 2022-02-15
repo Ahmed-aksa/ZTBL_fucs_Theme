ac4ba768bf874281c5278fecdae740d948ddb2dc
abdul.sami <abdul.sami@aksa-sds.com>
2022-02-15 16:38:47 +0500

    changes

--- src/app/modules/tour-dairy/tour-diary-approval-bm/tour-diary-approval-bm.component.ts
+++ src/app/modules/tour-dairy/tour-diary-approval-bm/tour-diary-approval-bm.component.ts
@@ -90,31 +90,6 @@ export class TourDiaryApprovalBmComponent implements OnInit {
         }
     }
 
-    onClearForm() {
-        // this.gridForm.controls['Name'].setValue("");
-        this.gridForm.controls['PPNO'].setValue("");
-        this.gridForm.controls['DiaryId'].setValue("");
-        this.gridForm.controls['TourPlanId'].setValue("");
-        this.gridForm.controls["ZoneId"].setValue(this.zone.ZoneId);
-        this.gridForm.controls["BranchId"].setValue(this.branch.BranchId);
-        this.gridForm.controls['TourDate'].setValue("");
-        this.gridForm.controls['DepartureFromPlace'].setValue("");
-        this.gridForm.controls['DepartureFromTime'].setValue("");
-        this.gridForm.controls['ArrivalAtPlace'].setValue("");
-        this.gridForm.controls['ArrivalAtTime'].setValue("");
-        // this.gridForm.controls['DisbNoOfCasesAppraised'].setValue("");
-        this.gridForm.controls['DisbNoOfNewBorrowerContacted'].setValue("");
-        this.gridForm.controls['RecNoOfDefaulterContacted'].setValue("");
-        this.gridForm.controls['DisbBorrowerRollOverCasesContacted'].setValue("");
-        this.gridForm.controls['RecAmountRecoveredWithLCNo'].setValue("");
-        this.gridForm.controls['NoOfUtilizationChecked'].setValue("");
-        this.gridForm.controls['AnyOtherWorkDone'].setValue("");
-        this.gridForm.controls['TOTFarmersContacted'].setValue("");
-        this.gridForm.controls['TOTNoOfFarmersVisisted'].setValue("");
-        this.gridForm.controls['AnyOtherWorkDone'].setValue("");
-        this.gridForm.controls['Remarks'].setValue("");
-        this.setValue();
-    }
 
     getAllData(event) {
         this.zone = event.final_zone;
