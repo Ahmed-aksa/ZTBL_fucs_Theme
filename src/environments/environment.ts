40458232fa85df6334de88b02c8e2d300db79063
abdulrehmanaxa1 <abdul.rehman@axabiztech.com>
2022-04-05 11:26:05 +0500

    code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,8 +12,8 @@ export const environment = {
     Profile_id: '57',
     // apiUrl: 'http://172.16.1.228:8070/ZTBL.Api', //admins
     // apiUrl: 'http://10.250.10.139/ZTBL.Apis', //mustansar_bhai_pc
-    apiUrl: 'http://10.1.103.102:8090/ZTBL.Api', //Ztbl DEV    ENV
-    // apiUrl: 'http://10.1.103.102:8091/UAT_APIS', //Ztbl UAT ENV,
+    //apiUrl: 'http://10.1.103.102:8090/ZTBL.Api', //Ztbl DEV    ENV
+     apiUrl: 'http://10.1.103.102:8091/UAT_APIS', //Ztbl UAT ENV,
     // apiUrl: 'http://10.1.32.105/ZTBL.Apis', //ZtblPCMustanser
     // apiUrl: 'http://10.1.33.158/ZTBL.Apis', //ZtblPCMustanser
     // apiUrl: 'https://localhost:44346/ZTBL.Api', //admins
@@ -35,80 +35,86 @@ export const environment = {
      * DEV
      */
 
-    MCO_Group_ID: '56',//131694
-    BM: '57',//80834
-    RECOVERY_OFFICER: '206',//120203
-    Regional_CHIEF: '203',//111573
-    ZM: '69',//81426
-    SVP_COD: '68',//112664
-    ZC: '75',//54964
-    PROVINCIAL_CHEIF: '205',//77345
-    EVP_CD: '200',//112664
-    SVP_RECOVERY: '199',//16284
-    EVP_OD: '202',//116035
-    EVP_RS: '201',//111537
-    PZ: '204',//704000
-    EVP_LMD: '207',//111537
-    ZC_BAHAWALNAGAR: '75',//68174
-    ZM_1: '75',//16284
-
-
-    '56': 'Maker',
-    '57': 'Branch Manager',
-    '206': 'Recovery Officer',
-    '203': 'Regional Chief',
-    '69': 'Zonal Manager',
-    '68': 'SVP COD',
-    '75': 'Zonal Chief',
-    '205': 'Provincial Chief',
-    '200': 'EVP CD',
-    '199': 'SVP Recovery',
-    '202': 'EVP OD',
-    '201': 'EVP RS',
-    '204': 'President',
-    '207': 'EVP LMD',
-    '55': 'Super Admin',
-    '60': 'Ho Monitoring',
-    '67': 'BIOMET',
-    '61': 'ZO MONITORING',
-    '76': 'ZONAL COMMITTE',
+    // MCO_Group_ID: '56',//131694
+    // BM: '57',//80834
+    // RECOVERY_OFFICER: '206',//120203
+    // Regional_CHIEF: '203',//111573
+    // ZM: '69',//81426 | 69996
+    // ZM_OPS: '181',//81426
+    // SVP_COD: '68',//112664
+    // ZC: '75',//54964
+    // PROVINCIAL_CHEIF: '205',//77345
+    // EVP_CD: '200',//112664
+    // SVP_RECOVERY: '199',//16284
+    // EVP_OD: '202',//116035
+    // EVP_RS: '201',//111537
+    // PZ: '204',//704000
+    // EVP_LMD: '207',//111537
+    // ZC_BAHAWALNAGAR: '75',//68174
+    // ZM_1: '75',//16284
+    //
+    //
+    // '56': 'Maker',
+    // '57': 'Branch Manager',
+    // '206': 'Recovery Officer',
+    // '203': 'Regional Chief',
+    // '69': 'Zonal Manager',
+    // '68': 'SVP COD',
+    // '75': 'Zonal Chief',
+    // '205': 'Provincial Chief',
+    // '200': 'EVP CD',
+    // '199': 'SVP Recovery',
+    // '202': 'EVP OD',
+    // '201': 'EVP RS',
+    // '204': 'President',
+    // '207': 'EVP LMD',
+    // '55': 'Super Admin',
+    // '60': 'Ho Monitoring',
+    // '67': 'BIOMET',
+    // '61': 'ZO MONITORING',
+    // '76': 'ZONAL COMMITTE',
+    // '181': 'ZONAL MANAGER OPS'
 
     /**
      * UAT
      */
-    // MCO_Group_ID: '56',//131694
-    // BM: '57',//80834
-    // RECOVERY_OFFICER: '214',//120203
-    // Regional_CHIEF: '206',//111573
-    // ZM: '69',//81426
-    // SVP_COD: '68',//112664
-    // ZC: '75',//54964
-    // PROVINCIAL_CHEIF: '208',//77345
-    // EVP_CD: '211',//112664
-    // SVP_RECOVERY: '213',//16284
-    // EVP_OD: '209',//116035
-    // EVP_RS: '210',//111537
-    // PZ: '207',//704000
-    // EVP_LMD: '212',//111537
-    // '56': 'Maker',
-    // '57': 'Branch Manager',
-    // '214': 'Recovery Officer',
-    // '206': 'Regional Chief',
-    // '69': 'Zonal Manager',
-    // '68': 'SVP COD',
-    // '75': 'Zonal Chief',
-    // '208': 'Provincial Chief',
-    // '211': 'EVP CD',
-    // '213': 'SVP Recovery',
-    // '209': 'EVP OD',
-    // '210': 'EVP RS',
-    // '207': 'President',
-    // '212': 'EVP LMD',
-    // '55':'Super Admin',
-    // '60':'Ho Monitoring',
-    // '67':'BIOMET',
-    // '61': 'ZO MONITORING',
-    // '76': 'ZONAL COMMITTE',
+
+    MCO_Group_ID: '56',//131694
+    BM: '57',//80834
+    RECOVERY_OFFICER: '214',//120203
+    Regional_CHIEF: '206',//111573
+    ZM: '69',//81426
+    SVP_COD: '68',//112664
+    ZC: '75',//54964
+    PROVINCIAL_CHEIF: '208',//77345
+    EVP_CD: '211',//112664
+    SVP_RECOVERY: '213',//16284
+    EVP_OD: '209',//116035
+    EVP_RS: '210',//111537
+    PZ: '207',//704000
+    EVP_LMD: '212',//111537
+    ZM_CAD: '78',//81426
+
+    '56': 'Maker',
+    '57': 'Branch Manager',
+    '214': 'Recovery Officer',
+    '206': 'Regional Chief',
+    '69': 'Zonal Manager',
+    '68': 'SVP COD',
+    '75': 'Zonal Chief',
+    '208': 'Provincial Chief',
+    '211': 'EVP CD',
+    '213': 'SVP Recovery',
+    '209': 'EVP OD',
+    '210': 'EVP RS',
+    '207': 'President',
+    '212': 'EVP LMD',
+    '55':'Super Admin',
+    '60':'Ho Monitoring',
+    '67':'BIOMET',
+    '61': 'ZO MONITORING',
+    '76': 'ZONAL COMMITTE',
+    '78': 'ZONAL MANAGER CAD'
 
 
 };
